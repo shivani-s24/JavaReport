7f9177ee351f8f2d5a13d703e82634d62fb51f22
Shivani Sathyanarayan <Shivani.S@ivycomptech.com>
2022-09-20 23:54:12 +0530

    First commit

---
+++ src/MysteryColorAnalyzer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+ interface MysteryColorAnalyzer {
+     int numberOfDistinctColors(List<Color> mysteryColors);
+
+     int colorOccurrence(List<Color> mysteryColors, Color color);
+
+     int numberOfDistinctcolor(List<Color> mystercolors);
+    int colorOccurence(List<Color> mysterycolors, Color color);
+}
